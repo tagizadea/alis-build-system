d54c55e5bb9f6bbfa4d0a92f841ed59315f7a1dd
tagizadea <tagizadea01@gmail.com>
2025-08-29 04:08:38 +0400

    Track system added
    - Now it is possible to track changed files using CRC32
    - Manager class added to manage .abs hidden directory

--- include/manager.hpp
+++ include/manager.hpp
@@ -6,7 +6,10 @@
 #include <fstream>
 #include <string>
 #include <cstdint>
+#include <vector>
+#include <unordered_map>
 
+using namespace std;
 namespace fs = std::filesystem;
 
 class Manager{
@@ -17,15 +20,16 @@ class Manager{
 
     public:
     
-    Manager& getInstance(){
+    static Manager& getInstance(){
         static Manager instance;
         return instance;
     }
-
-    
+    vector <string> sources;
+    unordered_map <string, pair <unsigned int, uint32_t>> tracked_files, current_files;
 };
 
 void init_manager();
+vector <string> track();
 
 
 #endif
--- src/manager.cpp
+++ src/manager.cpp
@@ -29,12 +29,98 @@ uint32_t compute_crc32(std::ifstream &file){
 
 void init_manager(){
     generate_crc32_table();
-    if(!fs::is_directory(".abs")){
-        fs::create_directory(".abs");
-        std::fstream tracked(".abs/tracked.txt", std::ios::out);
-        tracked << "SALAM";
+    if(!fs::is_directory(".abs")) fs::create_directory(".abs");
+
+    fstream tracked;
+    if(!fs::exists(".abs/tracked.txt")) tracked.open(".abs/tracked.txt", ios::in | ios::out | ios::app);
+    else tracked.open(".abs/tracked.txt", ios::in | ios::out);
+    if(!tracked.is_open()){
+        cout << "Manager error: The file \"tracked\" could not opened!";
+        exit(0); // !!! Debug systemi ile deyis
     }
-    else{
-        std::cout << "SALAM2";
+
+    fstream about(".abs/about.txt", ios::out);
+    if(!about.is_open()){
+        cout << "Manager error: The file \"about\" could not opened!";
+        exit(0); // !!! Debug systemi ile deyis
     }
+    about << "Ali's Build System for C/C++.\nVersion number: 0.3ALPHA";
+    about.close();
+
+    string line;
+    int line_cnt = 0;
+    while(getline(tracked, line)){
+        if(line.size() <= 2) continue;
+        string name = "", checksum = "";
+        bool flag = false;
+        for(int i=0;i<line.size();++i){
+
+            if(line[i] == ' '){
+                flag = true;
+                continue;
+            }
+
+            if(flag) checksum += line[i];
+            else name += line[i];
+        }
+        try{
+            Manager::getInstance().tracked_files[name] = {++line_cnt, static_cast<uint32_t>(stoul(checksum))};
+        }
+        catch(const std::exception& e){
+            cout << "Manager error: \"tracked\" file corrupted! " << e.what() << '\n';
+            exit(0); // !!! Debug systemi ile deyis
+        }
+        
+    }
+    tracked.close();
+    // for(auto i : Manager::getInstance().tracked_files) cout << i.first << ' ' << i.second.first << ' ' << i.second.second << '\n'; //
+
+}
+
+vector<string> track(){
+    vector <string> ans;
+
+    for(string s : Manager::getInstance().sources){
+        if(fs::exists(s)){
+            ifstream file(s);
+            uint32_t checksum = compute_crc32(file);
+            uint32_t saved_checksum = 0;
+            bool first_time = false;
+            if(Manager::getInstance().tracked_files.count(s))
+                saved_checksum = Manager::getInstance().tracked_files[s].second;
+            else first_time = true;
+            
+            if(checksum != saved_checksum){
+                ans.push_back(s);
+                Manager::getInstance().tracked_files[s].second = checksum;
+                if(first_time){
+                    fstream tracked(".abs/tracked.txt", ios::app);
+                    tracked << s << ' ' << checksum << '\n';
+                    tracked.close();
+                }
+                else{
+                    int line = Manager::getInstance().tracked_files[s].first;
+                    fstream tracked(".abs/tracked.txt", ios::in | ios::out);
+                    string l;
+                    int c_l = 0;
+                    vector <string> temp;
+                    while(getline(tracked, l)){
+                        ++c_l;
+                        if(c_l == line) temp.push_back( s + " " + to_string(checksum));
+                        else temp.push_back(l);
+                    }
+                    tracked.clear();
+                    tracked.seekp(0, ios::beg);
+                    for(string si : temp){
+                        tracked << si << '\n';
+                    }
+                    tracked.close();
+                }
+            }
+
+            file.close();
+        }
+    }
+
+    return ans;
 }
